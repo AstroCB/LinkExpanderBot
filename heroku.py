321f94642cdc77375ed692d8c94492f38c0d8b03
Cameron Bernhardt <cambernhardt@me.com>
2016-11-09 17:31:17 -0500

    Add logging

--- heroku.py
+++ heroku.py
@@ -1,10 +1,9 @@
 import os
 from flask import Flask
 
-print("Are you here")
 app = Flask(__name__)
 port = int(os.environ.get('PORT', 5000))
-app.run(host='0.0.0.0', port=port)
+@app.run(host='0.0.0.0', port=port)
 USERNAME = os.environ["USERNAME"]
 PASSWORD = os.environ["PASSWORD"]
 USERAGENT = os.environ["USERAGENT"]
